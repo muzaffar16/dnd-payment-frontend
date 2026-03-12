20e2b4303230050560c26be1510a44fe0762aa88
Muzaffar Ali <92566448+muzaffar16@users.noreply.github.com>
2026-03-13 04:47:45 +0500

    fix token

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-﻿window.PAYMENT_API_BASE_URL = 'https://payment-service.dropndash.co.uk';
+﻿window.PAYMENT_API_BASE_URL = 'https://backend.justhempit.co.uk';
 window.APPLE_PAY_MERCHANT_ID = 'merchant.com.app.dropndash';
 
 
