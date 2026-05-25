54871553d4c219de2856e27f27388115e746f610
Naveed Asad Ali <kjee6190@gmail.com>
2026-05-26 01:33:12 +0500

    fix

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.PAYMENT_API_BASE_URL = 'https://payment-service.dropndash.co.uk';
 // Trust site reference used by backend JWT init (not passed as Apple wallet merchant id)
 window.APPLE_PAY_SITE_REFERENCE = 'test_rrominternational130858';
 // Apple wallet merchant id for st.ApplePay merchantId (must be Apple-registered format, e.g. merchant.com.example)
-window.APPLE_PAY_MERCHANT_ID = '';
+window.APPLE_PAY_MERCHANT_ID = 'Jwt@rrominternational.com';
 window.APPLE_PAY_COUNTRY_CODE = 'GB';
 
 
