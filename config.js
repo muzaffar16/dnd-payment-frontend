681a24c3ff635b3c159909d548a7d03a8a01e108
Muzaffar Ali <92566448+muzaffar16@users.noreply.github.com>
2026-05-26 23:02:21 +0500

    redeploy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.PAYMENT_API_BASE_URL = 'https://payment-service.dropndash.co.uk';
 // Trust site reference used by backend JWT init and st.ApplePay merchantId
 window.APPLE_PAY_SITE_REFERENCE = 'test_rrominternational130858';
 // Apple Pay merchantId for st.ApplePay; Trust expects this to match the site reference
-window.APPLE_PAY_MERCHANT_ID = 'Jwt@rrominternational.com';
+window.APPLE_PAY_MERCHANT_ID = 'test_rrominternational130858';
 window.APPLE_PAY_COUNTRY_CODE = 'GB';
 
 
